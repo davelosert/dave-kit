fc1cf53c7a6f7a4e73dd808397bbee63bce72c84
David Losert <davelosert@github.com>
2022-09-21 10:10:17 +0200

    feat: Adds set-recording-size action

---
+++ scripts/set-recording-size.ts
@@ -0,0 +1,17 @@
+// Menu: Set Recording Size
+// Description: Use yabai to set a good size for all windows on the current space to start a video recording or screen sharing
+// Author: David Losert
+import "@johnlindquist/kit";
+
+// First make sure all windows are in floating mode
+await $`yabai -m space --layout float`;
+
+const allWindowsRaw = await $`yabai -m query --windows --space`;
+const allWindows: any[] = JSON.parse(allWindowsRaw.stdout);
+
+
+// 2560×1440
+for(const window of allWindows) {
+  await $`yabai -m window ${window.id} --grid 4:4:1:0:2:4`
+  await $`yabai -m window ${window.id} --resize abs:2560:1440`
+}
