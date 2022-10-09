af624827ba304b51432def044a4932b3d53551f3
David Losert <davelosert@github.com>
2022-10-09 20:23:56 +0200

    feat: Renames focusApp and adds one to cycle through

--- scripts/focusApp.ts
+++ scripts/focusApp.ts
@@ -1,15 +1,20 @@
-// Menu: [f]ocus app
-// Description: Use yabai to search and focus a running app
+// Menu: Yabai Focus App
+// Description: Use yabai to search and focus a app given
 // Author: David Losert
-// Shortcut: shift option f
 import "@johnlindquist/kit";
+
+const appName = await arg();
+console.log(`App name: ${appName}`);
 
 const allWindowsRaw = await $`yabai -m query --windows`;
 const allWindows = JSON.parse(allWindowsRaw.stdout);
 
-const selectedWindow = await arg('Select Window to open', allWindows.map(({ id, app, title }) => ({
-  name: `${app} - ${title}`,
-  value: id,
-})));
+const foundWindows = allWindows.filter(({ app }) => (app === appName));
 
-await $`yabai -m window --focus ${selectedWindow}`;
+const focusedWindowIndex = foundWindows
+  .sort((a, b) => a.id - b.id)
+  .findIndex(window => window['has-focus']);
+
+const nextIndex = (focusedWindowIndex + 1) % foundWindows.length;
+
+await $`yabai -m window ${foundWindows[nextIndex].id} --focus`;
