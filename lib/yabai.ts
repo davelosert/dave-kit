86eb122eeb596be8b0640d02ea3fd5b69b2a5844
David Losert <davelosert@github.com>
2023-08-26 17:01:32 +0200

    feat: Adds a windowPosition controller

--- lib/yabai.ts
+++ lib/yabai.ts
@@ -21,6 +21,18 @@ type YabaiDisplay = {
   index: number;
 }
 
+async function balanceSpace(spaceIndex: YabaiSpace | number) {
+  if(typeof spaceIndex !== 'number') {
+    spaceIndex = spaceIndex.index;
+  }
+  
+  try {
+    await $`yabai -m space ${spaceIndex} --balance`;
+  } catch (e) {
+    console.warn(`Could not balance space ${spaceIndex} due to error:`, e);
+  }
+}
+
 async function queryAllWindows(): Promise<YabaiWindow[]> {
   const allWindowsRaw = await $`yabai -m query --windows`;
   const allWindows = JSON.parse(allWindowsRaw.stdout);
@@ -37,6 +49,12 @@ async function queryAllSpaces(): Promise<YabaiSpace[]> {
   const allSpacesRaw = await $`yabai -m query --spaces`;
   const allSpaces = JSON.parse(allSpacesRaw.stdout);
   return allSpaces;
+}
+
+async function querySpace(spaceIndex: number): Promise<YabaiSpace> {
+  const spaceRaw = await $`yabai -m query --spaces --space ${spaceIndex}`;
+  const space = JSON.parse(spaceRaw.stdout);
+  return space;
 }
 
 async function queryAllDisplays(): Promise<YabaiDisplay[]> {
@@ -60,11 +78,29 @@ async function findFocusedWindow(allWindows?: YabaiWindow[]): Promise<YabaiWindo
   return searchWindows.find(({ 'has-focus': hasFocus }) => hasFocus);
 }
 
-async function moveWindowToSpaceIfExists(window: YabaiWindow, spaceIndex: number) {
+async function moveWindowToSpaceIfExists(window: YabaiWindow, space: YabaiSpace) {
+  if(space.windows.includes(window.id)) return;
+
   try {
-    await $`yabai -m window ${window.id} --space ${spaceIndex}`;
+    await $`yabai -m window ${window.id} --space ${space.index}`;
   } catch (e) {
-    console.warn(`Could not move window ${window.id} to space ${spaceIndex} due to error:`, e);
+    console.warn(`Could not move window ${window.id} to space ${space.index} due to error:`, e);
+  }
+}
+
+async function setInsert(window: YabaiWindow, direction: 'north' | 'east' | 'south' | 'west') {
+  try {
+    await $`yabai -m window ${window.id} --insert ${direction}`;
+  } catch (e) {
+    console.warn(`Could not insert window ${window.id} to direction ${direction} due to error:`, e);
+  }
+}
+
+async function warpWindow(sourceWindow: YabaiWindow, targetWindow: YabaiWindow) {
+  try {
+    await $`yabai -m window ${targetWindow.id} --warp ${sourceWindow.id}`;
+  } catch (e) {
+    console.warn(`Could not warp window ${sourceWindow.id} to window ${targetWindow.id} due to error:`, e);
   }
 }
 
@@ -76,16 +112,40 @@ async function moveWindowToDisplayIfExists(window: YabaiWindow, displayIndex: nu
   }
 }
 
+async function swapWindows(windowIndexA: number, windowIndexB: number) {
+  try {
+    await $`yabai -m window ${windowIndexA} --swap ${windowIndexB}`;
+  } catch (e) {
+    console.warn(`Could not swap windows ${windowIndexA} and ${windowIndexB} due to error:`, e);
+  }
+}
+
+async function ensureSpaceLayout(space: YabaiSpace, layout: 'bsp' | 'float' | 'stack') {
+  if(space.type === layout) return;
+
+  try {
+    await $`yabai -m space ${space} --layout ${layout}`;
+  } catch (e) {
+    console.warn(`Could not set layout ${layout} on space ${space} due to error:`, e);
+  }
+}
+
 const yabai = {
+  balanceSpace,
   queryAllWindows,
   queryAllWindowsOnSpace,
   queryAllSpaces,
   queryAllDisplays,
   queryCurrentSpace,
+  querySpace,
   findApp,
   findFocusedWindow,
   moveWindowToSpaceIfExists,
-  moveWindowToDisplayIfExists
+  moveWindowToDisplayIfExists,
+  ensureSpaceLayout,
+  setInsert,
+  swapWindows,
+  warpWindow
 };
 
 export {
@@ -93,5 +153,6 @@ export {
 };
 
 export type {
-  YabaiWindow
+  YabaiWindow,
+  YabaiSpace
 };
---
+++ scripts/windowPositions.ts
@@ -0,0 +1,122 @@
+// Menu: [w]indow positions
+// Description: Use yabai to switch the layout mode
+// Author: David Losert
+import "@johnlindquist/kit";
+import { yabai, YabaiWindow, YabaiSpace } from '../lib/yabai';
+
+const mode = await arg('Select Window Definition Scheme', [
+  {
+    name: '[w]ork',
+    value: 'work'
+  },
+  {
+    name: '[c]ode',
+    value: 'code'
+  },
+  {
+    name: '[d]emo',
+    value: 'demo'
+  }
+]);
+
+const remainingWindows = await yabai.queryAllWindows();
+
+const findWindowAndMarkManaged = (query): YabaiWindow | null => {
+  const windowIndex = remainingWindows.findIndex(window => {
+    let appMatch = query.app ? false : true;
+    let titleMatch = query.title ? false : true;
+
+    if(query.app && query.app === window.app) appMatch = true;
+    if(query.title && query.title === window.title) titleMatch = true;
+    
+    return appMatch && titleMatch;
+  });
+  
+  if(windowIndex === -1) return null;
+
+  return remainingWindows.splice(windowIndex, 1)[0];
+}
+
+async function putWindowsVerticallyOnSpace(windows: YabaiWindow[], space: YabaiSpace) {
+  const existingWindows = windows.filter(window => Boolean(window));
+  const isFirstWindowOnSpace = space.windows.includes(existingWindows[0].id);
+  
+  if(!isFirstWindowOnSpace) {
+    await yabai.moveWindowToSpaceIfExists(windows[0], space);
+    // Overwrite the space information to get the most recent information
+    space = await yabai.querySpace(space.index);
+  }
+
+  const isFirstWindow = space['first-window'] === existingWindows[0].id;
+  if(!isFirstWindow) {
+    await yabai.swapWindows(existingWindows[0].id, space['first-window']);
+  }
+  
+  
+  for(let i = 1; i < existingWindows.length; i++) {
+    const previousWindow = existingWindows[i-1];
+    const currentWindow = existingWindows[i];
+    await yabai.setInsert(previousWindow, 'east');
+    await yabai.warpWindow(previousWindow, currentWindow);
+  }
+}
+
+const spaces = await yabai.queryAllSpaces();
+const  slack = findWindowAndMarkManaged({ app: 'Slack'}),
+  chrome = findWindowAndMarkManaged({ app: 'Google Chrome'}),
+  code = findWindowAndMarkManaged({ app: 'Code'}),
+  outlook = findWindowAndMarkManaged({ app: 'Microsoft Outlook'}),
+  teams = findWindowAndMarkManaged({ app: 'Microsoft Teams'}),
+  signal = findWindowAndMarkManaged({ app: 'Signal'}),
+  discord = findWindowAndMarkManaged({ app: 'Discord'}),
+  spotify = findWindowAndMarkManaged({ app: 'Spotify'}),
+  todoist = findWindowAndMarkManaged({ app: 'Todoist'});
+
+
+if(mode === 'work') {
+  // First move all not explicitly mentioned windows to space 3
+  for(const window of remainingWindows) {
+    await yabai.moveWindowToSpaceIfExists(window, spaces[2]);
+  }
+
+  // SPACE 1
+  await yabai.ensureSpaceLayout(spaces[0], 'bsp')
+  await putWindowsVerticallyOnSpace([code, chrome, slack], spaces[0]);
+
+  // SPACE 2
+  await yabai.ensureSpaceLayout(spaces[1], 'bsp')
+  await putWindowsVerticallyOnSpace([outlook, teams, signal, discord], spaces[1]);
+  
+  // MOVE SPOTIOFY TO SECOND LAST SPACE
+  await putWindowsVerticallyOnSpace([spotify], spaces.at(-2));
+  // MOVE TODOIST TO LAST SPACE
+  await putWindowsVerticallyOnSpace([todoist], spaces.at(-1));
+
+  // Ensure all spaces are evenly balanced in the end
+  await yabai.balanceSpace(spaces[0]);
+  await yabai.balanceSpace(spaces[1]);
+}
+
+if(mode === 'code') {
+  // First move all not explicitly mentioned windows to space 3
+  for(const window of remainingWindows) {
+    await yabai.moveWindowToSpaceIfExists(window, spaces[2]);
+  }
+
+  // SPACE 1
+  await yabai.ensureSpaceLayout(spaces[0], 'bsp')
+  await putWindowsVerticallyOnSpace([chrome, code, slack], spaces[0]);
+
+  // SPACE 2
+  await yabai.ensureSpaceLayout(spaces[1], 'bsp')
+  await putWindowsVerticallyOnSpace([outlook, teams, signal, discord], spaces[1]);
+  
+  // MOVE SPOTIOFY TO SECOND LAST SPACE
+  await putWindowsVerticallyOnSpace([spotify], spaces.at(-2));
+  // MOVE TODOIST TO LAST SPACE
+  await putWindowsVerticallyOnSpace([todoist], spaces.at(-1));
+
+  // Ensure all spaces are evenly balanced in the end
+  await yabai.balanceSpace(spaces[0]);
+  await yabai.balanceSpace(spaces[1]);
+}
